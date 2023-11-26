61fb43fe276eaab06801dfb3f798309f6a24e5cf
Aman Sharma <amansharma505152@gmail.com>
2023-11-27 01:37:47 +0530

    GolfSite-First Commit

---
+++ script.js
@@ -0,0 +1,52 @@
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top=dets.y+"px"
+    blur.style.left=dets.x -250 +"px"
+    blur.style.top=dets.y-250+"px"
+})
+
+
+
+gsap.to("#nav",{
+    backgroundColor : '#000',
+    height:'110px',
+    duration:0.5,
+    scrollTrigger:{
+        trigger:'#nav',
+        scroller:'body',
+        // markers:true,
+        start:'top -10%',
+        end:'top -11%',
+        scrub:1
+    }
+})
+
+gsap.to("#main",{
+    backgroundColor: "#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        // markers:true,
+        start:"top -25%",
+        end:"top -70%",
+        scrub:2
+    }
+})
+
+gsap.from("#aboutus img,#about-us-in", {
+    y: 90,
+    opacity: 0,
+    duration: 1,
+    stagger:0.3,
+    scrollTrigger: {
+      trigger: "#aboutus",
+      scroller: "body",
+      // markers:true,
+      start: "top 70%",
+      end: "top 65%",
+      scrub: 1,
+    },
+  });
